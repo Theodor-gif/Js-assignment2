e3eb956979f33ad6347759e39a2cda1bac22c004
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-09-15 19:18:22 +0200

    Create the HTML for the calculator

---
+++ js/calculator.js
@@ -0,0 +1,19 @@
+"use strict"; // I am using to deffine some errors
+
+/* 
+TASKS
+
+1. add , subtract , multiply and divide
+2. Create the functions: add() , subtract(), multiply() and divide()
+3. Create the function operate(num1, num2) , then call add(), sybtract(), multiply() or divide()
+4. Html includes buttons for each digit and "Equals"/"clear" button.
+5. Create a display the values
+6. Numbers: 1, 2, 3, 4, 5, 6, 7, 8, 9
+7. Symbols: +, -, *, /, ., =, clear btn and delete btn
+
+*/
+
+// Variables
+
+const firstScreen = document.querySelector("#firstScreen");
+const secondScreen = document.querySelector("#secondScreen");
